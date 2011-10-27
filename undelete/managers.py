93f53ad4e5d9c1e8bf61e33fa8de255f4ba4c615
Sandro Salles <sandro@localhost.snippet.com.br>
2011-10-27 15:02:38 -0200

    Added Model TrashedItem

--- undelete/managers.py
+++ undelete/managers.py
@@ -4,10 +4,10 @@ class NonTrashedManager(models.Manager):
     ''' Query only objects which have not been trashed. '''
     def get_query_set(self):
         query_set = super(NonTrashedManager, self).get_query_set()
-        return query_set.filter(trashed_at__isnull=True)
+        return query_set.filter(trashed=False)
 
 class TrashedManager(models.Manager):
     ''' Query only objects which have been trashed. '''
     def get_query_set(self):
         query_set = super(TrashManager, self).get_query_set()
-        return query_set.filter(trashed_at__isnull=False)
+        return query_set.filter(trashed=True)
--- undelete/models.py
+++ undelete/models.py
@@ -1,23 +1,56 @@
 from datetime import datetime
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import generic
 from undelete.managers import TrashedManager, NonTrashedManager
 
-class TrashableMixin(models.Model):
-	trashed_at = models.DateTimeField(_('Trashed'), editable=False, blank=True, null=True)
+
+class TrashedItem(models.Model):
+	trashed_at = models.DateTimeField(_('Trashed at'), editable=False, blank=True, null=True)
+
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = generic.GenericForeignKey('content_type', 'object_id')
+    
+    def save(*args, **kwargs):
+    	self.content_object.trashed = True
+    	self.content_object.save()
+
+    	super(TrashedItem, self).save(*args, **kwargs)
+
+    def __unicode__(self):
+    	return self.content_object	
+	    
+
+class TrashableModel(models.Model):
 
 	objects = NonTrashedManager()
 	trash = TrashedManager()
+	
+	trashed = models.BooleanField(_("Trashed"), default=False)
 
 	def delete(self, *args, **kwargs, trash=True):
-	    if not self.trashed_at and trash:
-	        self.trashed_at = datetime.now()
-	        self.save()
+	    if not self.trashed and trash:
+	    	trashed_item = TrashedItem(content_object=self, trashed_at=datetime.now())
+	    	trashed_item.save()
 	    else:
-			super(SomeModel, self).delete(*args, **kwargs)
+			super(TrashableModel, self).delete(*args, **kwargs)
+
 
 	def restore(self, commit=True):
-		self.trashed_at = None
+		trashed_items = TrashedItem.objects.filter(content_object=self)
+
+		for item in trashed_items:
+			item.delete()
+		
+		self.trashed = False
+
 		if commit:
 			self.save()
+	
+	class Meta:
+		abstract = True
+
+
+
